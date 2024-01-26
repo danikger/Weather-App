c65508871e5f6d64f38df1f7aec2408254c75c40
Daniil Gerachshenko <danikger@gmail.com>
2024-01-25 19:24:05 -0600

    Changes font to Inter and changes tailwind config
    Includes:
    - Changes font to Inter
    - Adds reverse spinning animation to tailwind config file
    - Adds aspect-ratio tailwind plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,39 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'reverse-spin': 'reverse-spin 1s linear infinite'
+      },
+      keyframes: {
+        "reverse-spin": {
+          from: {
+            transform: 'rotate(360deg)'
+          },
+        }
+      }
+    },
+    fontFamily: {
+      sans: [
+        'Inter',
+        'system-ui',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        '"Noto Sans"',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+        '"Noto Color Emoji"',
+      ],
+    },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+  ],
 }
 
