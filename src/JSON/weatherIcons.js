cdf0c3dab838dd14cdc4354fcb0a9ff8362f384a
Daniil Gerachshenko <danikger@gmail.com>
2024-02-03 23:06:51 -0600

    Adds potential weather conditions and icons
    Adds an array of potential weather conditions and icons for each condition.
    JSON from: https://www.weatherapi.com/docs/weather_conditions.json

---
+++ src/JSON/weatherIcons.js
@@ -0,0 +1,202 @@
+import { RiSunLine, RiDrizzleLine, RiThunderstormsLine, RiCloudLine, RiRainyLine, RiSnowyLine, RiMoonClearLine, RiMistLine, RiFoggyLine } from "react-icons/ri";
+
+const weatherIcons = [
+    {
+        weather: "Sunny",
+        icon: RiSunLine,
+    },
+    {
+        weather: "Clear",
+        icon: RiMoonClearLine,
+    },
+    {
+        weather: "Partly cloudy",
+        icon: RiCloudLine,
+    },
+    {
+        weather: "Cloudy",
+        icon: RiCloudLine,
+    },
+    {
+        weather: "Overcast",
+        icon: RiCloudLine,
+    },
+    {
+        weather: "Mist",
+        icon: RiMistLine,
+    },
+    {
+        weather: "Patchy rain possible",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Patchy snow possible",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Patchy sleet possible",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Patchy freezing drizzle possible",
+        icon: RiDrizzleLine,
+    },
+    {
+        weather: "Thundery outbreaks possible",
+        icon: RiThunderstormsLine,
+    },
+    {
+        weather: "Blowing snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Blizzard",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Fog",
+        icon: RiFoggyLine,
+    },
+    {
+        weather: "Freezing fog",
+        icon: RiFoggyLine,
+    },
+    {
+        weather: "Patchy light drizzle",
+        icon: RiDrizzleLine,
+    },
+    {
+        weather: "Light drizzle",
+        icon: RiDrizzleLine,
+    },
+    {
+        weather: "Freezing drizzle",
+        icon: RiDrizzleLine,
+    },
+    {
+        weather: "Heavy freezing drizzle",
+        icon: RiDrizzleLine,
+    },
+    {
+        weather: "Patchy light rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Light rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate rain at times",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Heavy rain at times",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Heavy rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Light freezing rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate or heavy freezing rain",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Light sleet",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate or heavy sleet",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Patchy light snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Light snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Patchy moderate snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Moderate snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Patchy heavy snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Heavy snow",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Ice pellets",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Light rain shower",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate or heavy rain shower",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Torrential rain shower",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Light sleet showers",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Moderate or heavy sleet showers",
+        icon: RiRainyLine,
+    },
+    {
+        weather: "Light snow showers",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Moderate or heavy snow showers",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Light showers of ice pellets",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Moderate or heavy showers of ice pellets",
+        icon: RiSnowyLine,
+    },
+    {
+        weather: "Patchy light rain with thunder",
+        icon: RiThunderstormsLine,
+    },
+    {
+        weather: "Moderate or heavy rain with thunder",
+        icon: RiThunderstormsLine,
+    },
+    {
+        weather: "Patchy light snow with thunder",
+        icon: RiThunderstormsLine,
+    },
+    {
+        weather: "Moderate or heavy snow with thunder",
+        icon: RiThunderstormsLine,
+    }
+]
+
+export default weatherIcons;
